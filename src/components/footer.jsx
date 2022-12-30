d2b803ea0c675ba199c90f0b9a4a4602e108060a
Rilega <leandroriosga@gmail.com>
2022-12-30 00:53:25 -0300

    modifico header, footer y sus estilos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,32 +1,37 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+
 
 const Footer =() =>{
   return (
     <footer>
+    <div class="logo-footer">
+        <a class="logo" href="/"><img src={logo} alt='' /></a>
+    </div>
     <section class="info">
         <ul>
             <h2>SOBRE NOSOTROS</h2>
-            <li><a href="">About SoundSurge</a></li>
-            <li><a href="">Mayoristas</a></li>
-            <li><a href="">Sucursales</a></li>
-            <li><a href="">Preguntas frecuentes</a></li>
+            <li><a href="/aboutUs">About <br/> SoundSurge</a></li>
+            <li><a href="">Preguntas <br/> frecuentes</a></li>
         </ul>
         <ul>
             <h2>NUESTROS LOCALES</h2>
-            <li><a href="https://goo.gl/maps/jgLkJ2YGPHFVgKx17" target="_blank">Sucursal Lanus</a></li>
-            <li><a href="https://g.page/rockcenter?share" target="_blank">Sucursal C.A.B.A</a></li>
-            <li><a href="https://goo.gl/maps/RUZwgessDfVcAD4u6" target="_blank">Sucursal Morón</a></li>
-            <li><a href="https://goo.gl/maps/ptPoEctEW94BJj2n9" target="_blank">Sucursal San Miguel</a></li>
+            <li><a id="ubi1">Sucursal <br/> Lanus</a></li>
+            <li><a id="ubi2" >Sucursal <br/> San Miguel</a></li>
         </ul>
+    </section>
+    <section class="ubicacion">
+        <div id="mapa" class="mapa">
+        </div>
     </section>
         <h2>REDES</h2>
     <section class="Redes-sociales">
         <ul>
-            <li><a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook-square"></i></a></li>
-            <li><a href="https://www.instagram.com/soundsurgearg/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-            <li><a href="https://www.whatsapp.com/" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
-            <li><a href="https://www.linkedin.com/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-            <li><a href="https://mail.google.com/mail/u/0/?tab=rm#inbox?compose=CllgCJfttZZpkVbZCXfXnxCCSBjJPLglQDKnNTKgXCzQlKSZGSVkXNjCpwvBsPkkFClpmSdBvRg" target="_blank"><i class="fas fa-at" ></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-whatsapp"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fas fa-at" ></i></a></li>
         </ul>
     </section>
 </footer>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,72 +4,89 @@ import logo from '../assets/logo.png'
 const Header = () => {
   return (
     <header>
-        <section class="navegacion">
-            <input type="checkbox" name="burguer" id="burguer"/>
-            <label class="burguer" for="burguer"><i class="fas fa-bars"></i></label>
-            <nav class="opcionesDePerfil-Movil">
+    <section class="navegacion">
+        <input type="checkbox" name="burguer" id="burguer"/>
+        <label class="burguer" for="burguer"><i class="fas fa-bars"></i></label>
+        <nav class="opcionesDePerfil-Movil">
             <ul>
-               <li><a class="btn-neon" href="/admin/lista">Admin</a></li>
+                
+                    <li><a class="btn-neon" href="/users/perfil">Perfil</a></li>
                     
+                        <li><a class="btn-neon" href="/admin/lista">Admin</a></li>
+                    
+               
                     <li><a class="btn-neon" href="/users/login">login</a></li>
                     <li><a class="btn-neon" href="/users/register">Register</a></li> 
-               
-                <li><a class="btn-neon" href="#">Categorias</a></li>
-                <li><a class="btn-neon" href="#">Marcas</a></li> 
-                <li><a class="btn-neon" href="#">Lo + vendido</a></li>
-                 <form class="exit" action="/users/logout?_method=DELETE" method="post">
+                
+                <li><a class="btn-neon" href="/productos/todos">Todos los productos</a></li>
+                <li><a class="btn-neon" href="/productos/loMasVendido">Lo + Vendido</a></li>
+                
+                    <form class="exit" action="/users/logout?_method=DELETE" method="post">
                         <button class="logout" type="submit">Cerrar Sesion</button> 
                     </form>
-               
-            </ul>
-            </nav>
-            <img src={logo} alt='' />
-            <section>
-            <ul class="opcionesDeProductos">
-                <li><a class="btn-neon" href="/productos/categorias">Categorias</a></li>
-                <li><a class="btn-neon" href="/productos/marcas">Marcas</a></li> 
-                <li><a class="btn-neon" href="/productos/loMasVendido">Lo + vendido</a></li>
-            </ul>
-            </section>
-            <form action="/busqueda" method="get">
-                <div class="busqueda">
-                <input id="search" name="search" type="text" placeholder="¿Que estas Buscando?" />
-                <button type="submit"><i class="fas fa-search"></i></button>
-                </div>
-            </form>
-            <div class="carrito">
-            <a href="/productos/carrito" target="_blank" class="btn-neon"><i class="fas fa-shopping-cart"></i></a>
-            </div>
-            <div class="usuario">
-                <input type="checkbox" name="perfil" id="perfil"/>   
                 
-                    <input type="checkbox" name="perfil" id="perfil"/>
-                    <label class="perfilU" for="perfil"><img class="usuario"  /><h2>Hola!</h2>
-                    </label>
-            
-                    <input type="checkbox" name="perfil" id="perfil"/>
-                    <label class="sinLog" for="perfil"><i class="fas fa-user-circle"></i><h2>Hola!</h2></label>
-                
-                <nav class="opcionesDePerfil">
-                    <ul>
-                        
-                            <li><a class="btn-neon" href="/users/perfil">Perfil</a></li>
-                            
-                                <li><a class="btn-neon" href="/admin/lista">Admin</a></li>
+            </ul>
+        </nav>
+        <a class="logo" href="/"><img src={logo} alt='' /></a>
 
-                            <form class="exit" action="/users/logout?_method=DELETE" method="post">
-                            <button class="logout" type="submit">Cerrar Sesion</button> 
-                            </form>
-                        
-                            <li><a class="btn-neon" href="/users/login">login</a></li>
-                            <li><a class="btn-neon" href="/users/register">Register</a></li> 
-                    
-                        
-                    </ul>
-                </nav>
+        <section class="busqueda">
+            <div>
+                <form class="form-search" action="/busqueda" method="get">
+                    <input type="text" name="search" id="search" placeholder="¿Que estas Buscando?"/>
+                    <button type="submit"><i class="fas fa-search"></i></button>
+                </form>
             </div>
         </section>
-    </header>
+            
+        
+            <div class="carrito1">
+                <a href="/productos/carrito" target="_blank" class="btn-neon"><i class="fas fa-shopping-cart"></i></a>
+            </div>
+        
+                
+       
+        <div class="usuario">
+            <input type="checkbox" name="perfil" id="perfil"/>   
+            
+                <input type="checkbox" name="perfil" id="perfil"/>
+                <label class="perfilU btn-neon" for="perfil"><img src={logo} alt='' /><h2>Hola!</h2>
+                </label>
+        
+            <nav class="opcionesDePerfil">
+                <ul>
+                    
+                        <li><a class="btn-neon" href="/users/perfil">Perfil</a></li>
+                        
+                            <li><a class="btn-neon" href="/admin/lista">Admin</a></li>
+                        
+                        <form class="exit" action="/users/logout?_method=DELETE" method="post">
+                        <button class="logout" type="submit">Cerrar Sesion</button> 
+                        </form>
+                    
+                        <li><a class="btn-neon" href="/users/login">Ingresar</a></li>
+                        <li><a class="btn-neon" href="/users/register">Registrarse</a></li> 
+                    
+                    
+                </ul>
+            </nav>
+        </div>
+    </section>
+
+    <section class="opcionesDeProductos">
+        <ul class="opcionesDeProductos">
+            
+
+           
+            <li><a class="btn-neon" href="/productos/todos">To2 los productos</a></li>
+            <li><a class="btn-neon" href="/AboutUs">Somos SoudSurge</a></li>
+            <li><a class="btn-neon" href="/productos/loMasVendido">Lo + Vendido</a></li>
+        </ul>
+        
+    </section>
+    
+    
+    
+</header>
   )
 }
 
